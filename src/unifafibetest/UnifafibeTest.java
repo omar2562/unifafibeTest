bf4df0ef339f97a68f1f0830679c70e2f88af844
Andrey Omar Mozo Uscamayta <omar2562@gmail.com>
2020-11-25 18:37:32 -0300

    init

---
+++ src/unifafibetest/UnifafibeTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unifafibetest;
+
+/**
+ *
+ * @author omar2
+ */
+public class UnifafibeTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Test");
+    }
+    
+}
