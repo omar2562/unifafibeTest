d977c8f47647baffba9e3796f54d69945ef28898
Andrey Omar Mozo Uscamayta <omar2562@gmail.com>
2020-11-25 20:07:10 -0300

    test4

--- src/unifafibetest/UnifafibeTest.java
+++ src/unifafibetest/UnifafibeTest.java
@@ -19,6 +19,7 @@ public class UnifafibeTest {
         System.out.println("Test");
         System.out.println("Test2");
         System.out.println("Test3");
+        System.out.println("Test4");
     }
     
 }
